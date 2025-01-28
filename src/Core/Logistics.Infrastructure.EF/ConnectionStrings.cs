2a2698eda1660fcfda3fd38ed8f15e9fbd9ae47f
Nyasha Shawn Majoto <shawnjots@gmail.com>
2025-01-28 16:27:00 +0200

    feat: db config

--- src/Core/Logistics.Infrastructure.EF/ConnectionStrings.cs
+++ src/Core/Logistics.Infrastructure.EF/ConnectionStrings.cs
@@ -2,6 +2,6 @@
 
 internal static class ConnectionStrings
 {
-    public const string LocalDefaultTenant = "Server=localhost; Database=default_logistics; Uid=root; Pwd=root; Connect Timeout=10";
-    public const string LocalMain = "Server=localhost; Database=main_logistics; Uid=root; Pwd=root; Connect Timeout=10";
+    public const string LocalDefaultTenant = "Server=102.37.17.93; Database=default_logistics; Uid=root; Pwd=your_new_password; Connect Timeout=15";
+    public const string LocalMain = "Server=102.37.17.93; Database=main_logistics; Uid=root; Pwd=your_new_password; Connect Timeout=15";
 }
